934d7a9cc265326e6593432f61b7c04d0e5baa39
Badhon63 <jianrobiul7898@gmail.com>
2026-03-31 20:26:44 +0600

    added cart section (items is now shown inside cart)

--- src/components/hompage/products/selectedproducts/SelectedProducts.jsx
+++ src/components/hompage/products/selectedproducts/SelectedProducts.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import SelectedCard from '../../ui/SelectedCard';
+import { toast } from 'react-toastify';
 
-const SelectedProducts = () => {
+const SelectedProducts = ({ selectedProducts, setSelectedProducts, setCartCount, cartCount }) => {
+
+  const handleRemove = (product) => {
+    const filtered = selectedProducts.filter(p => p.id !== product.id);
+    setSelectedProducts(filtered);
+    setCartCount(cartCount - 1);
+    toast.error(`${product.name} removed from cart!`);
+  }
+
+  const handleCheckout = () => {
+    setSelectedProducts([]);
+    setCartCount(0);
+    toast.success('Checkout successful!');
+  }
+
   return (
-    <div>
-      SelectedProducts
+    <div className='mt-10'>
+      {selectedProducts.length === 0
+        ? <div className='h-100 flex items-center justify-center flex-col gap-4'>
+          <h2 className='font-semibold text-xl'>Your cart is empty</h2>
+          <p>Go to Products tab to add items</p>
+        </div>
+        : <div className='space-y-4 border border-stone-200 p-8 rounded-2xl'>
+          <p className='text-2xl font-semibold'>Your Cart</p>
+          {selectedProducts.map((product, index) =>
+            <SelectedCard
+              key={index}
+              product={product}
+              handleRemove={handleRemove}
+            />
+          )}
+          <div className='flex justify-between items-center '>
+            <p className=''>Total:</p>
+              
+              <p className='font-bold text-xl'>${selectedProducts.reduce((total, product) => total + product.price, 0).toFixed(2)}</p>
+            
+          </div>
+          <button className='btn bg-linear-to-r from-[#632EF7] to-[#9415FA] text-white rounded-3xl w-full' onClick={handleCheckout}>Proceed to Checkout</button>
+        </div>
+      }
     </div>
   );
 };
--- src/components/hompage/ui/SelectedCard.jsx
+++ src/components/hompage/ui/SelectedCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
 
-const SelectedCard = () => {
+const SelectedCard = ({ product, handleRemove }) => {
   return (
-    <div>
-      
+    <div className='flex items-center justify-between p-6 rounded-2xl bg-base-200'>
+      <div className='flex items-center gap-4'>
+        <span className='text-4xl'>{product.icon}</span>
+        <div>
+          <h2 className='font-semibold text-xl'>{product.name}</h2>
+          <p className='text-sm opacity-60 font-semibold'>${product.price}</p>
+        </div>
+      </div>
+      <div className='flex items-center gap-4 jube'>
+        <button className='btn text-red-500' onClick={() => handleRemove(product)}>
+          Remove
+        </button>
+      </div>
     </div>
   );
 };
